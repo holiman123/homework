d2f84b0717f2a48ec337c2aa6150399ab6d10709
holiman123 <43368212+holiman123@users.noreply.github.com>
2018-09-26 22:17:48 +0300

    Add files via upload

---
+++ months/src/comcompany/Main.java
@@ -0,0 +1,52 @@
+package comcompany;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("src/comcompany/input.txt"));
+        switch (sc.nextInt()) {
+            case 1:
+                System.out.println("Январь");
+                break;
+            case 2:
+                System.out.println("Февраль");
+                break;
+            case 3:
+                System.out.println("Март");
+                break;
+            case 4:
+                System.out.println("Апрель");
+                break;
+            case 5:
+                System.out.println("Май");
+                break;
+            case 6:
+                System.out.println("Июнь");
+                break;
+            case 7:
+                System.out.println("Июль");
+                break;
+            case 8:
+                System.out.println("Август");
+                break;
+            case 9:
+                System.out.println("Сеньтябрь");
+                break;
+            case 10:
+                System.out.println("Октябрь");
+                break;
+            case 11:
+                System.out.println("Ноябрь");
+                break;
+            case 12:
+                System.out.println("Декабрь");
+                break;
+            default:
+                System.out.println("Извените, но на земле всего 12 месяцев!");
+        }
+    }
+}
